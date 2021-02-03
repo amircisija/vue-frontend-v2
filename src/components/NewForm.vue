1b6733c58570a53bd7d0ad8f6b2fd59cc8ee3200
Amir <amircisija@gmail.com>
2021-02-03 22:13:23 +0100

    removed files, exported schema js

--- src/components/NewForm.vue
+++ src/components/NewForm.vue
@@ -45,6 +45,8 @@
   Vue.use(VueFormGenerator)
   import axios from "axios";
 
+  import fields from './schema.js';
+
   export default {
     data() {
       return {
@@ -54,83 +56,7 @@
           message: '',
           terms: true
         },
-        schema: {
-          fields: [{
-              type: 'input',
-              inputType: 'text',
-              label: 'First Name',
-              model: 'firstname',
-              placeholder: 'Your first name',
-              required: true,
-              validator: ['string']
-            },
-            {
-              type: 'input',
-              inputType: 'text',
-              label: 'Last Name',
-              model: 'lastname',
-              placeholder: 'Your last name',
-              required: true,
-              validator: ['string']
-            },
-            {
-              type: "radios",
-              label: "Sex",
-              model: "gender",
-              values: [
-                "Male",
-                "Female"
-              ],
-              required: true
-            },
-            {
-              type: "textArea",
-              label: "Message",
-              model: "message",
-              name: "message",
-              placeholder: "Your Message",
-              rows: 4,
-              required: true,
-              validator: ['string']
-            },
-            {
-              type: "select",
-              selectOptions: {
-                noneSelectedText: 'Please Choose'
-              },
-              label: "This message is confidential",
-              model: "confidential",
-              values: [
-                "No",
-                "Yes"
-              ],
-              default: "No",
-              value: "No",
-              required: true,
-              validator: ['required'],
-              visible: (model) => {
-                if (model.message.length > 30) {
-                  return true;
-                }
-                return false;
-              }
-            },
-            {
-              type: 'checkbox',
-              label: 'I accept the TOS',
-              model: 'terms',
-              default: true,
-              required: true,
-              validator: ['required'],
-              visible: (model) => {
-                if (model.message.length > 30 && model.confidential) {
-                  return true;
-                }
-                return false;
-              }
-            },
-          ]
-        },
+        schema: fields,
         formOptions: {
           validateAfterLoad: true,
           validateAfterChanged: true,
@@ -178,10 +104,10 @@
           })
       }
     },
-
        created() {
           this.getFormData();
           this.isVisible = false;
+
         }
   }
 
